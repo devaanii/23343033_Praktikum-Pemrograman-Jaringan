5514c5624a53ec1fdca9dd1078f3cc10194605da
devaanii <devani1705@gmail.com>
2025-10-26 12:25:01 +0700

    js6

---
+++ Jobsheet 6 - HTTP Endpoints/src/app.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/prediksiCuaca')
+
+const app = express()
+
+//Mendefinisikan jalur/path untuk konfigurasi Express
+const direktoriPublik = path.join(__dirname, '../public')
+const direktoriViews = path.join(__dirname, '../templates/views')
+const direktoriPartials = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine dan lokasi folder views
+app.set('view engine', 'hbs')
+app.set('views', direktoriViews)
+hbs.registerPartials(direktoriPartials)
+
+//Setup direktori statis
+app.use(express.static(direktoriPublik))
+
+//halaman utama
+app.get('', (req, res) => {
+    res.render('index', {
+        judul: 'Aplikasi Cek Cuaca', 
+        nama: 'Devani' })
+})
+
+app.get('/bantuan', (req, res) => {
+    res.render('bantuan', {
+        judul: 'Bantuan',
+        teksBantuan: 'Ini adalah halaman bantuan',
+        nama: 'Devani'
+    })
+})
+
+app.get('/infoCuaca', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Kamu harus memasukkan lokasi yang ingin dicari.'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({error})
+        }
+        forecast(latitude, longitude, (error, dataPrediksi) => {
+            if (error) {
+                return res.send({error})
+            }
+            res.send({
+                prediksiCuaca: dataPrediksi,
+                lokasi: location,
+                alamat: req.query.address
+            })
+        })
+    })
+})
+
+// halaman tentang
+app.get('/tentang', (req, res) => {
+    res.render('tentang', {
+        judul: 'Tentang Saya',
+        nama: 'Devani'
+    })
+})
+
+app.get('/bantuan/', (req, res) => {
+    res.render('404', {
+        judul: '404',
+        nama: 'Devani',
+        pesanKesalahan: 'Artikel yang dicari tidak ditemukan.'
+    })
+})
+
+// wildcard untuk semua halaman lain
+app.use((req, res) => {
+    res.render('404', {
+        judul: '404',
+        nama: 'Devani',
+        pesanKesalahan: 'Halaman tidak ditemukan.'
+    })
+})
+
+app.listen(4000, () => {
+    console.log('Server berjalan pada port 4000')
+})
